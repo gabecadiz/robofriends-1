094f553541c588f5a0ad19c47df39617f50ce614
Computer <gabecadiz@hotmail.com>
2019-06-26 18:00:11 -0400

    created error boundary component to wrap cardlist component

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,22 +2,24 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
+  //comment below is for testing error boundary 
+  // if (true) {
+  //   throw new Error('Noooo!');
+  // }
   return (
     <div>
-      {
-        robots.map((user, i) => {
-          return (
-            <Card
-              key={i}
-              id={robots[i].id}
-              name={robots[i].name}
-              email={robots[i].email}
-              />
-          );
-        })
-      }
+      {robots.map((user, i) => {
+        return (
+          <Card
+            key={i}
+            id={robots[i].id}
+            name={robots[i].name}
+            email={robots[i].email}
+          />
+        );
+      })}
     </div>
   );
-}
+};
 
 export default CardList;
---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+export default class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false,
+      error: '',
+      errorInfo: ''
+    };
+  }
+  //if anything errors out, this lifecycle hook gets ran
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div>
+          <h2>Something went wrong.</h2>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
